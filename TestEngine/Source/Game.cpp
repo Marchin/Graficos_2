46caa5db7a6f7851549147a66b6f4e4475e59706
Marchin <mmonterrosa94@gmail.com>
2018-11-05 11:08:52 -0300

    Remove comments from game loop

--- TestEngine/Source/Game.cpp
+++ TestEngine/Source/Game.cpp
@@ -68,50 +68,52 @@ bool Game::OnStop() {
 
 bool Game::OnUpdate() {
 	m_counter++;
-	//m_pTriangle->Translate(0.0f, 0.0f, 0.0f);
-	//m_pTriangle->Scale(sin(m_counter*0.01f), 1.f, sin(m_counter*0.01f));
-	//m_pTriangle->Scale(3.f, sin(m_counter*0.01f) * 3.f, 1.f);
-	//m_pTriangle->RotateX(sin(m_counter*0.01f) * 180.f / 3.1415f);
-	//m_pTriangle->RotateY(sin(m_counter*0.01f) * 180.f / 3.1415f);
-	//m_pTriangle->RotateZ(sin(m_counter*0.01f) * 180.f / 3.1415f);
-	////m_pTriangle->Draw();
-	//if (m_counter == 255) {
-	//	float squareColors[] = {
-	//		1.f, 0.f, 0.f,
-	//		1.f, 0.f, 1.f,
-	//		1.f, 1.f, 1.f,
-	//		0.f, 1.f, 0.f
-	//	};
-	//	m_pColorSquare->SetColors(&squareColors);
-	//}
+	m_pTriangle->SetPosition(-2.0f, -2.0f, 0.0f);
+	m_pTriangle->Scale(sin(m_counter*0.01f), 1.f, sin(m_counter*0.01f));
+	m_pTriangle->Scale(3.f, sin(m_counter*0.01f) * 3.f, 1.f);
+	m_pTriangle->RotateX(sin(m_counter*0.01f) * 180.f / 3.1415f);
+	m_pTriangle->RotateY(sin(m_counter*0.01f) * 180.f / 3.1415f);
+	m_pTriangle->RotateZ(sin(m_counter*0.01f) * 180.f / 3.1415f);
+	m_pTriangle->Draw();
+	if (m_counter == 255) {
+		float squareColors[] = {
+			1.f, 0.f, 0.f,
+			1.f, 0.f, 1.f,
+			1.f, 1.f, 1.f,
+			0.f, 1.f, 0.f
+		};
+		m_pColorSquare->SetColors(&squareColors);
+	}
 
 
-	//if (m_counter == 125) {
-	//	float squareVertices[] = {
-	//		-1.0f, -0.5f, 0.f,
-	//		-0.5f,  0.5f, 0.f,
-	//		 0.5f, -0.5f, 0.f,
-	//		 0.5f,  1.0f, 0.f
-	//	};
-	//	m_pColorSquare->SetVertices(&squareVertices);
-	//	m_pTriangle->SetVertices(&squareVertices);
-	//}
+	if (m_counter == 125) {
+		float squareVertices[] = {
+			-1.0f, -0.5f, 0.f,
+			-0.5f,  0.5f, 0.f,
+			 0.5f, -0.5f, 0.f,
+			 0.5f,  1.0f, 0.f
+		};
+		m_pColorSquare->SetVertices(&squareVertices);
+		m_pTriangle->SetVertices(&squareVertices);
+	}
 
-	//m_pColorSquare->RotateY(sin(m_counter*0.001f) * 180.f / 3.1415f);
-	//m_pColorSquare->Scale(3.f, 3.f, 3.f);
-	//m_pColorSquare->SetPosition(2.f, 2.f, 0.f);
-	////m_pColorSquare->Draw();
+	m_pColorSquare->RotateY(sin(m_counter*0.001f) * 180.f / 3.1415f);
+	m_pColorSquare->Scale(3.f, 3.f, 3.f);
+	m_pColorSquare->SetPosition(2.f, 2.f, 0.f);
+	m_pColorSquare->Draw();
 
-	//if (m_counter % 25 == 0) {
-	//	m_sidesCounter++;
-	//	m_sidesCounter %= 15;
-	//}
-	//m_pCircle->SetSidesAmount(m_sidesCounter);
-	//m_pCircle->SetRadius(m_sidesCounter / 10.f);
-	////m_pCircle->Draw();
+	if (m_counter % 25 == 0) {
+		m_sidesCounter++;
+		m_sidesCounter %= 15;
+	}
+	m_pCircle->SetSidesAmount(m_sidesCounter);
+	m_pCircle->SetRadius(m_sidesCounter / 10.f);
+	m_pCircle->SetPosition(-2.f, 2.f, 0.f);
+	m_pCircle->Draw();
 
-	////m_pSprite->Scale(3.f, 3.f, 1.f);
-	////m_pSprite->Draw();
+	m_pSprite->SetPosition(2.f, -2.f, 0.f);
+	m_pSprite->Scale(3.f, 3.f, 1.f);
+	m_pSprite->Draw();
 
 	if (m_counter == 1000) {
 		m_counter = 0;
